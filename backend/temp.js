6aa0a1c8eb2ae117f280a7ec72560eae3b7fef97
Fadhil Rohman <fadhil.rohman10@gmail.com>
2026-08-08 18:41:23 +0700

    fix: hapus file sampah test.js yang merusak railway

--- backend/temp.js
+++
@@ -1,8 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-
-async function main() {
-  const shifts = await prisma.shift.findMany({ select: { id: true, openedAt: true, closedAt: true } });
-  console.log(shifts);
-}
-main().finally(() => prisma.$disconnect());
